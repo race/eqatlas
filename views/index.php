a54dce3cb1132ca267f7a52f1678f2895cef7849
Jeremy Race <hello@jeremyrace.ca>
2020-08-15 11:55:04 -0700

    [NoID] - move news to its own element

--- views/index.php
+++ views/index.php
@@ -267,72 +267,8 @@
   </tr>
 </table>
 
-<table cellspacing=1 border=0 width=90% align="center">
-  <tr>
-    <td>
+<?php include('./views/elements/news.php'); ?>
 
-      <div class="footnotes">
-        <br/><br/>
-        <hr>
-        <br/><br/>
-
-        <h1><a name=notes>Notes</a></h1>
-        <hr/>
-        <h2>Nav. Improvements Under Development</h2>
-        <p class="date">Aug 15, 2020</p>
-        <p>EQAtlas.ca is undergoing a bunch of backend work to help template-ize our archaeic mapbook. The design will remain intact for the exhilerating nostalgia of classic EQAtlas &lt;3. With the maps templated, headers and pages can be dynamically created by the server so we'll be able to link to connected zones directly from the map pages. This means as you travel from Butcherblock Mountains to Kaladim you won't need to go back to the Index page (or the map detail page) - you'll be able to flow directly to the next map from your current map!</p>
-        <p>We are going to start with some of the most commonly used zones and expand from there:
-          <ul>
-            <li>Greater Faydark</li>
-            <li>Butcherblock Mountains</li>
-            <li>Kaladim (North and South)</li>
-            <li>West Commonlands</li>
-            <li>East Commonlands</li>
-            <li>East Karana</li>
-            <li>West Karana</li>
-            <li>North Karana</li>
-          </ul>
-        </p>
-        <p>On the more technical side of things, the following are being done:
-          <ul>
-            <li>Content is being moved into a database for easier updates</li>
-            <li>Pages to be generated dynamically</li>
-            <li>URLs will be more friendly</li>
-            <li>Links to Project1999 pages will be added over time</li>
-          </ul>
-        </p>
-
-        <p class="signature-we">- Serynn Dipity</p>
-        <hr/>
-        <h2>Broken Links Fixed</h2>
-        <p class="date">Mar 5, 2020</p>
-        <p>We noticed we had a few pages giving an error or broken links, these should be correctly removed/cleaned up now.</p>
-        <p class="signature-we">- Serynn Dipity</p>
-        <hr/>
-        <h2>Maps Post-Velious Removed</h2>
-        <p class="date">Oct 10, 2016</p>
-        <p>We decided to remove any maps that were Post-Velious as they aren't used for the classic / Project1999 experience anyway. Less is more, they say ;-)</p>
-        <p>For those who don't want to embrace the Lord of Hate, you can still access them via the "<a href="/atlas.html">Ye Old Atlas</a>" page.</p>
-        <p class="signature-de">- Tydel Shadow`Shifter</p>
-        <hr/>
-        <h2>EQAtlas.ca is now fully encrypted!</h2>
-        <p class="date">Nov 6, 2015</p>
-        <p>Here at EQAtlas.ca, we are proud to sssupport initiativesss that focusss on internet freedom and sssafety - as a ressult we've enabled full encryption for our audience. Now even you can join The Faceless in the Temple of Terror. Enjoy!</p>
-        <p>Our encryption certificatesss are provided by Let's Encrypt - for more information on this exciting initiatve, visit: <a href="https://letsencrypt.org/" target="_blank">letsencrypt.org</a></p>
-        <p class="signature-ik">- Sacraxiles Shadow`Spirit</p>
-        <br/>
-        <hr>
-        <br/>View Source: <a href="https://github.com/EQAtlas/eqatlas" target="_blank">https://github.com/EQAtlas/eqatlas</a>
-        <br/><br/>
-        <br/><br/><br/><br/>
-      </div>
-
-    </td>
-  </tr>
-</table>
-
-<?php
-/*
 <script>
   (function (i, s, o, g, r, a, m) {
     i['GoogleAnalyticsObject'] = r;
@@ -350,7 +286,6 @@
   ga('send', 'pageview');
 
 </script>
-*/
-?>
+
 <link href='https://fonts.googleapis.com/css?family=Great+Vibes' rel='stylesheet' type='text/css'>
 <link href='https://fonts.googleapis.com/css?family=Arizonia' rel='stylesheet' type='text/css'>
